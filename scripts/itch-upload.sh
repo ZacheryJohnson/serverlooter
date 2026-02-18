8c058f2cdda7e0927c12eda7118b457fdd394606
Zach Johnson <15884513+ZacheryJohnson@users.noreply.github.com>
2026-02-17 21:08:50 -0800

    Fix itch upload script excluding assets

--- scripts/itch-upload.sh
+++ scripts/itch-upload.sh
@@ -4,16 +4,24 @@ ITCHIO_USERNAME="determinismdev"
 ITCHIO_GAME="serverlooter"
 ITCHIO_CHANNEL="web"
 
-WORKING_DIR="${1:-.}"
+WORKING_DIR="${1:-$(pwd)}"
 OUT_DIR="$WORKING_DIR/wasm_out"
 OUT_NAME="serverlooter"
 GIT_HASH=$(git rev-parse --short HEAD)
 OUT_ZIP=$OUT_DIR/$OUT_NAME-itch-$GIT_HASH.zip
 
-zip $OUT_ZIP \
-  $OUT_DIR/serverlooter-${GIT_HASH}.js \
-  $OUT_DIR/serverlooter_opt-${GIT_HASH}.wasm \
-  $OUT_DIR/index.html
+cp -r $WORKING_DIR/assets $OUT_DIR/assets
+
+cd $OUT_DIR
+
+zip -r $OUT_ZIP \
+  serverlooter-${GIT_HASH}.js \
+  serverlooter_opt-${GIT_HASH}.wasm \
+  index.html \
+  assets
+
+cd $WORKING_DIR
+rm -r $OUT_DIR/assets
 
 butler push $OUT_ZIP $ITCHIO_USERNAME/$ITCHIO_GAME:$ITCHIO_CHANNEL
 
