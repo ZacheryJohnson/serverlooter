fc5cdd06096bd75007559fef2167ae66857b0892
Zach Johnson <15884513+ZacheryJohnson@users.noreply.github.com>
2026-03-08 19:32:20 -0700

    Remove cd from upload script

--- scripts/itch-upload.sh
+++ scripts/itch-upload.sh
@@ -8,31 +8,27 @@ WORKING_DIR="${1:-$(pwd)}"
 OUT_DIR="$WORKING_DIR/wasm_out"
 OUT_NAME="serverlooter"
 GIT_HASH=$(git rev-parse --short HEAD)
-OUT_ZIP=$OUT_DIR/$OUT_NAME-itch-$GIT_HASH.zip
+OUT_ZIP=$OUT_NAME-itch-$GIT_HASH.zip
 BUTLER_EXE="${2:-butler}"
 
 cp -r $WORKING_DIR/assets $OUT_DIR/assets
 
 echo "Verifying OUT_DIR before zip:"
 ls -l $OUT_DIR
-cd $OUT_DIR || exit 1
-echo "Verifying files before zip:"
-ls -l serverlooter-${GIT_HASH}.js serverlooter_opt-${GIT_HASH}.wasm index.html assets/
 
-zip -MM -r $OUT_ZIP \
-  serverlooter-${GIT_HASH}.js \
-  serverlooter_opt-${GIT_HASH}.wasm \
-  index.html \
-  assets/
+zip -r $OUT_DIR/$OUT_ZIP \
+  $OUT_DIR/serverlooter-${GIT_HASH}.js \
+  $OUT_DIR/serverlooter_opt-${GIT_HASH}.wasm \
+  $OUT_DIR/index.html \
+  $OUT_DIR/assets
 
 if [ $? -ne 0 ]; then
-  echo "failed to zip all requested files; aborting."
+  echo "failed to zip all requested files (errno=$?); aborting."
   exit 1
 fi
 
-cd $WORKING_DIR
 rm -r $OUT_DIR/assets
 
-$BUTLER_EXE push $OUT_ZIP $ITCHIO_USERNAME/$ITCHIO_GAME:$ITCHIO_CHANNEL
+$BUTLER_EXE push $OUT_DIR/$OUT_ZIP $ITCHIO_USERNAME/$ITCHIO_GAME:$ITCHIO_CHANNEL
 
-rm $OUT_ZIP
+rm $OUT_DIR/$OUT_ZIP
